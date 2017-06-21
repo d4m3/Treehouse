182896e1486bb4fb316da7a071e8147a4c7c8254
DForbes <damianoforbes@gmail.com>
2017-06-20 20:54:17 -0400

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import FormView, CreateView
+
+from . import forms
+
+
+
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
+
+
+# Logout form layout
+class LogoutView(LoginRequiredMixin, FormView):
+    form_class = forms.LogoutForm
+    template_name = 'users/logout.html'
+
+    def form_valid(self, form):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('home'))
+
+
+# Sign up form layout
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('users:dashboard')
