5caabdf3a9cbf86500ee619613db466c24110d35
DForbes <damianoforbes@gmail.com>
2017-06-21 19:25:38 -0400

    added graphs with chartist

--- thoughts/templatetags/thought_tags.py
+++ thoughts/templatetags/thought_tags.py
@@ -1,7 +1,12 @@
+import datetime
 from django import template
+from django.utils import timezone
+import json
+
 
 # thoughts.forms
 from ..forms import ThoughtForm
+
 
 register = template.Library()
 
@@ -10,3 +15,17 @@ register = template.Library()
 def thought_form():
     form = ThoughtForm()
     return {'form': form }
+
+# Pushing data to chart on dashboard
+@register.simple_tag(takes_context=True)
+def chart_data(context):
+    user = context['user']
+    ten_days_ago = timezone.now() - datetime.timedelta(days=10)
+    thoughts = user.thoughts.filter(recorded_at__gte=ten_days_ago)
+    return json.dumps({
+        'labels' : [thought.recorded_at.strftime('%Y-%m-%d') for
+                    thought in thoughts],
+        'series' : [[thought.condition for thought in thoughts]]
+    })
+
+
