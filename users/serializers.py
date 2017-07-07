fdf57652dc8af181c5ca9b1119bf1a5167690717
DForbes <damianoforbes@gmail.com>
2017-07-07 19:13:00 -0400

    User creation throught API

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,27 @@
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+
+from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.HiddenField(default='')
     class Meta:
         model = User
-        fields = ('username', )
+        fields = ('username','first_name','last_name','email',
+                  'last_login','date_joined','password')
 
+        read_only_fields = ('last_login','date_joined')
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    # Handles the creation on password from Postman
+    # Password wasn't created
+    def create(self, validated_data):
+        validated_data['password'] = self.context['request'].data['password']
+        user = User.objects.create_user(**validated_data)
+        return user
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+    # Allow username to be created from Postman
+    def update(self, instance, validated_data):
+        try:
+            del validated_data['username']
+        except KeyError:
+            pass
+        return super().update(instance, validated_data)
