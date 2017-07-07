68df881be0c33ca42bcd511d387623dd1d9094bd
DForbes <damianoforbes@gmail.com>
2017-07-07 16:41:46 -0400

    recorded_at is read-only. Create uses requesting users

--- thoughts/serializers.py
+++ thoughts/serializers.py
@@ -7,5 +7,14 @@ class ThoughtSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Thought
         fields = ('recorded_at', 'condition', 'notes')
+        read_only_fields = ('recorded_at',)
+
+    def create(self, validated_data):
+        import pdb
+        pdb.set_trace()
+        thought = models.Thought(**validated_data)
+        thought.user = self.context['request'].user
+        thought.save()
+        return thought
 
 
--- thoughts/viewsets.py
+++ thoughts/viewsets.py
@@ -8,3 +8,8 @@ class ThoughtViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.thoughts.all()
+
+    # def create(self, request, *args, **kwargs):
+    #     import pdb
+    #     pdb.set_trace()
+    #     return super().create(request, *args, **kwargs)
